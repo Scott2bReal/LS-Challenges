f855bc5fe4e2a4fb40830a28e004cc81fcc9b77c
Scott Hoecker <hoeckers@gmail.com>
2022-02-06 08:51:38 -0600

    Finish octals challenge

--- easy/octals/octal.rb
+++ easy/octals/octal.rb
@@ -1,5 +1,28 @@
 class Octal
-  def initialize(decimal_number)
-    @input = decimal_number
+  VALID_OCTAL_DIGITS = %w(0 1 2 3 4 5 6 7)
+
+  def initialize(input)
+    @input = input
+  end
+
+  def to_decimal
+    return 0 unless valid_octal?
+
+    digits = @input.to_s.chars.reverse
+
+    converted = digits.map.with_index do |digit, idx|
+      digit.to_i * (8**idx)
+    end
+
+    converted.sum
+  end
+
+  private
+
+  def valid_octal?
+    @input.to_s.chars.each do |digit|
+      return false unless VALID_OCTAL_DIGITS.include?(digit)
+    end
+    true
   end
 end
