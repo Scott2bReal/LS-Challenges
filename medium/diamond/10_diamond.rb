e23ae88977f9e7d1ea9599f7e2185fb4a83c9046
Scott Hoecker <hoeckers@gmail.com>
2022-02-14 09:41:40 -0600

    Finish diamonds

--- medium/diamond/10_diamond.rb
+++ medium/diamond/10_diamond.rb
@@ -1,18 +1,52 @@
 class Diamond
+  ALPHABET = ('A'..'Z').to_a
+
   class << self
     def make_diamond(letter)
-      # Make outside_buffer
-      # Make a center buffer
-      # For each letter in letters
-      #  make a line with current letter and buffers
-      #  add to output_diamond
-      #  increment center buffer, decrement outide buffers
-      final_diamond = ''
-      letters = ('A'..letter).to_a
+      range = make_range_array(letter)
+      width = determine_width(letter)
+      diamond = []
+      range.each { |char| diamond << make_row(char).center(width) }
+      diamond.join("\n") + "\n"
     end
+
+    private
 
     def determine_width(letter)
       return 1 if letter == 'A'
+      determine_spaces(letter).size + 2
+    end
+
+    def make_row(letter)
+      case letter
+      when 'A' then 'A'
+      when 'B' then 'B B'
+      else
+        "#{letter}#{determine_spaces(letter)}#{letter}"
+      end
+    end
+
+    def determine_spaces(letter)
+      case letter
+      when 'A' then ''
+      when 'B' then ' '
+      when 'C' then '   '
+      else
+        range = ('D'..letter)
+        spaces = '   '
+        range.each { spaces += '  ' }
+        spaces
+      end
+    end
+
+    def make_range_array(letter)
+      full_range = []
+      up_range = ('A'..letter).to_a
+      up_range.each { |char| full_range << char }
+      up_range.reverse_each { |char| full_range << char unless char == letter }
+      full_range
     end
   end
 end
+
+Diamond.make_diamond('E')
