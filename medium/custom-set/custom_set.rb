0f1c2bba921783c7c68249d1c38441c77bfbc4fe
Scott Hoecker <hoeckers@gmail.com>
2022-02-17 07:09:48 -0600

    Finish challenges

--- medium/custom-set/custom_set.rb
+++ medium/custom-set/custom_set.rb
@@ -1,41 +1,67 @@
 class CustomSet
-  def initialize(items=nil)
-    if items
-      @items = items
-    else
-      @items = []
-    end
+  def initialize(items=[])
+    @items = items.uniq
   end
 
   def add(item)
     @items.push(item)
+    @items.uniq!
+    self
   end
 
   def empty?
     @items.empty?
   end
 
-  def contains?(other_set)
-    return true if empty? && other_set.empty?
-    @items.include?(other_set)
+  def contains?(value)
+    @items.include? value
   end
 
   def subset?(other_set)
-    contains?(other_set)
+    @items.each do |item|
+      return false unless other_set.contains?(item)
+    end
+
+    true
   end
 
-  def disjoint?
+  def disjoint?(other_set)
+    return true if empty? || other_set.empty?
+    @items.each do |item|
+      return false if other_set.contains?(item)
+    end
+    true
   end
 
-  def eql?
+  def eql?(other_set)
+    @items.sort == other_set.items.sort
   end
 
-  def intersection
+  def ==(other_set)
+    eql?(other_set)
   end
 
-  def difference
+  def intersection(other_set)
+    @items.each_with_object(CustomSet.new) do |item, intersection_set|
+      intersection_set.add(item) if other_set.contains?(item)
+    end
   end
 
-  def union
+  def difference(other_set)
+    @items.each_with_object(CustomSet.new) do |item, difference_set|
+      difference_set.add(item) unless other_set.contains?(item)
+    end
   end
+
+  def union(other_set)
+    union_set = CustomSet.new
+    [self, other_set].each do |set|
+      set.items.each { |item| union_set.add(item) }
+    end
+    union_set
+  end
+  
+  protected
+
+  attr_accessor :items
 end
